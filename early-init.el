07d33510700c8d7f4798a0bdfbcc55ae0cbe5c95
Lajos Bodnar <bodnarlajoska@gmail.com>
2021-04-04 22:11:51 +0200

    Remove package.el initialization

--- early-init.el
+++ early-init.el
@@ -40,22 +40,6 @@
 
 (straight-use-package 'selectrum)
 
-;; ;;__________________________________________________________
-;; ;; For using Melpa and Elpa
-;; (custom-set-variables
-;;  ;; custom-set-variables was added by Custom.
-;;  ;; If you edit it by hand, you could mess it up, so be careful.
-;;  ;; Your init file should contain only one such instance.
-;;  ;; If there is more than one, they won't work right.
-;;  '(highlight-indent-guides-method 'bitmap)
-;;  '(package-selected-packages
-;;    '(selectrum zoom treemacs company-quickhelp lsp-ui lsp-mode haskell-mode lsp-haskell flycheck tide web-mode js2-mode undo-tree smex smartparens rg hydra highlight-indent-guides haskell-mode eglot dumb-jump diff-hl company)))
-
-;; (setq package-archives '(("gnu" . "https://elpa.gnu.org/packages/")
-;;                          ("melpa" . "https://melpa.org/packages/")))
-
-;; (setq package-quickstart t)
-
 (setq frame-inhibit-implied-resize t)
 
 (defun my/set-font ()
