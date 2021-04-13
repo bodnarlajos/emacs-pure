f262cd43da7f7dd5e90c908e18e93056baf5f3a0
lbodnar-wfscorp <lbodnar@wfscorp.com>
2021-04-13 08:57:34 +0200

    win_main: Fix contants in my-const.el

--- progs/my-const.el
+++ progs/my-const.el
@@ -1,5 +1,5 @@
-(defvar my/project-dir '("/home/lbodnar/Projects"))
-(defvar my/exec-dir "/home/lbodnar/.local/bin")
+(defvar my/project-dir '("c:/Projects"))
+(defvar my/exec-dir "c:/ProgramData/chocolatey/bin")
 (defvar my/notes-dir "c:/users/lbodnar/Box/notes/")
 
 (provide 'my-const)
