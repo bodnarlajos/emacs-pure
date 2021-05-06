4f26832e6a4e58a721d0f10cce65e63059dccf83
lbodnar-wfscorp <lbodnar@wfscorp.com>
2021-05-06 10:29:16 +0200

    win_main: Add one more path to exec-path

--- progs/my-const.el
+++ progs/my-const.el
@@ -1,5 +1,5 @@
 (defvar my/project-dir '("c:/Projects"))
-(defvar my/exec-dir '("C:/msys64/mingw64/bin" "c:/ProgramData/chocolatey/bin"))
+(defvar my/exec-dir '("C:/msys64/mingw64/bin" "c:/ProgramData/chocolatey/bin" "C:/msys64/usr/bin"))
 (defvar my/notes-dir "c:/users/lbodnar/Box/notes/")
 
 (provide 'my-const)
