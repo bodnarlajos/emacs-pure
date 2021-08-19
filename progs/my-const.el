61b501edc180822a7884c1ba99dcff8ad06fc37f
Lajos Bodnar <lbodnar@debla.debian>
2021-08-19 21:28:56 +0200

    xref

--- progs/my-const.el
+++ progs/my-const.el
@@ -1,5 +1,8 @@
-(defvar my/project-dir '("c:/Projects"))
-(defvar my/exec-dir '("C:/msys64/mingw64/bin" "c:/ProgramData/chocolatey/bin" "C:/msys64/usr/bin"))
-(defvar my/notes-dir "c:/users/lbodnar/Box/notes/")
+;; (defvar my/project-dir '("c:/Projects"))
+(defvar my/project-dir '("/home/lbodnar/Projects"))
+(defvar my/exec-dir '("/home/lbodnar/.local/bin"))
+;; (defvar my/exec-dir '("C:/msys64/mingw64/bin" "c:/ProgramData/chocolatey/bin" "C:/msys64/usr/bin"))
+;; (defvar my/notes-dir "c:/users/lbodnar/Box/notes/")
+(defvar my/notes-dir "/home/lbodnar/Documents/notes/")
 
 (provide 'my-const)
