2c31ba221657a484fd0323895922b991b00fbee1
Lajos Bodnar <lbodnar@wfscorp.com>
2021-08-25 15:38:59 +0200

    win spec commit

--- custom.el
+++ custom.el
@@ -6,7 +6,7 @@
  '(consult-buffer-filter
 	 '("\\` " "\\`\\*Completions\\*\\'" "\\`\\*Flymake log\\*\\'" "\\`\\*Semantic SymRef\\*\\'" "\\`\\*Messages\\*\\'" "\\`\\*straight.*\\*\\'" "\\`\\*tramp/.*\\*\\'"))
  '(custom-safe-themes
-	 '("66f32da4e185defe7127e0dc8b779af99c00b60c751b0662276acaea985e2721" "a99fb53a1d22ce353cab8db2fe59353781c13a4e1d90455f54f7e60c061bc9f4" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476")))
+	 '("a5371f7b6a18f06bda955bbe457e2c0946c85f263bbd4f610d6ac4f8c8d00feb" "66f32da4e185defe7127e0dc8b779af99c00b60c751b0662276acaea985e2721" "a99fb53a1d22ce353cab8db2fe59353781c13a4e1d90455f54f7e60c061bc9f4" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "fa2b58bb98b62c3b8cf3b6f02f058ef7827a8e497125de0254f56e373abee088" "bffa9739ce0752a37d9b1eee78fc00ba159748f50dc328af4be661484848e476")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- progs/my-const.el
+++ progs/my-const.el
@@ -2,6 +2,6 @@
 (defvar my/exec-dir '("c:/ProgramData/Git/usr/bin" "c:/ProgramData/chocolatey/bin" "c:/ProgramData/Git/mingw64/bin"))
 (defvar my/notes-dir "c:/users/lbodnar/Box/notes/")
 (setq my/autostart-dev-env t)
-(defvar my/base-dir "/home/lbodnar/Projects")
+(defvar my/base-dir "c:/Projects")
 
 (provide 'my-const)
