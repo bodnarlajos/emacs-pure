87a8e7a83c7cc28c8fffb8e845a58cc42f14d93e
Bodnar Lajos <bodnarlajoska@gmail.com>
2021-09-02 15:05:44 +0200

    Add defaults to init, and browser setting to constants

--- init.el
+++ init.el
@@ -29,31 +29,78 @@
       read-file-name-completion-ignore-case t
       read-buffer-completion-ignore-case t)
 
+(load (expand-file-name (concat user-emacs-directory "custom.el")))
+(require 'my-defun)
+(require 'my-keys)
+(require 'my-layout2)
+
 (straight-use-package 'selectrum)
 (straight-use-package 'selectrum-prescient)
 (straight-use-package 'consult)
-(selectrum-mode +1)
-(selectrum-prescient-mode +1)
-(prescient-persist-mode +1)
-
-(load (expand-file-name (concat user-emacs-directory "custom.el")))
-(require 'my-defun)
 (straight-use-package 'rg)
 (straight-use-package 'undo-tree)
-(global-undo-tree-mode)
 (straight-use-package 'smex)
-(require 'my-keys)
-(require 'my-setq-defaults)
-(require 'my-layout2)
 (straight-use-package 'ctrlf)
 (straight-use-package 'markdown-mode)
 (straight-use-package 'transpose-frame)
 (straight-use-package 'doom-modeline)
 
+(selectrum-mode +1)
+(selectrum-prescient-mode +1)
+(prescient-persist-mode +1)
+(global-undo-tree-mode)
 (doom-modeline-mode +1)
 (ctrlf-mode +1)
 (show-paren-mode +1)
 (recentf-mode)
+
+;; set defaults
+(setq-default
+ dired-dwim-target t
+ autoload-compute-prefixes nil
+ frame-inhibit-implied-resize t
+ initial-major-mode 'fundamental-mode
+ find-file-visit-truename nil
+ ffap-machine-p-known 'accept     ; don't ping things that look like domain names
+ idle-update-delay 1              ; update ui slightly less often
+ ;; History & backup settings (save nothing, that's what git is for)
+ auto-save-default nil
+ create-lockfiles nil
+ history-length 500
+ make-backup-files nil  ; don't create backup~ files
+ package-check-signature nil
+ custom-file (concat user-emacs-directory "custom.el")
+ inhibit-startup-screen t
+ tab-width 2
+ split-height-threshold 0
+ split-width-threshold nil
+ bidi-paragraph-direction 'left-to-right
+ bidi-display-reordering nil
+ gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3"
+ browse-url-browser-function 'browse-url-generic
+ set-mark-command-repeat-pop t
+ completion-styles '(partial-completion substring intials flex)
+ resize-mini-windows t
+ completions-format 'vertical
+ isearch-allow-scroll t
+ scroll-preserve-screen-position t
+ isearch-allow-prefix t
+ linum-format "%3s"
+ mark-ring-max 32)
+
+;; Better scrolling
+(setq redisplay-dont-pause t
+      scroll-margin 1
+      scroll-step 1
+      scroll-conservatively 10000
+      scroll-preserve-screen-position 1)
+(set-window-scroll-bars (minibuffer-window) nil nil)
+
+;; do not split window
+(setq split-width-threshold (- (window-width) 10))
+(setq split-height-threshold nil)
+
+(defconst *spell-check-support-enabled* nil) ;; Enable with t if you prefer
 
 (add-hook 'nxml-mode-hook 'my/long-line)
 (add-hook 'json-mode-hook 'my/long-line)
