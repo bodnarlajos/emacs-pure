8c2f561e7f5b65eb34ea4e31bd3d1e426f93aec4
Lajos Bodnar <lbodnar@wfscorp.com>
2021-09-02 13:56:30 +0200

    Add theme to custom.el

--- custom.el
+++ custom.el
@@ -5,6 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(consult-buffer-filter
 	 '("\\` " "\\`\\*Completions\\*\\'" "\\`\\*Flymake log\\*\\'" "\\`\\*Semantic SymRef\\*\\'" "\\`\\*Messages\\*\\'" "\\`\\*straight.*\\*\\'" "\\`\\*tramp/.*\\*\\'"))
+ '(custom-safe-themes
+	 '("58fb295e041032fd7a61074ca134259dfdef557ca67d37c4240dbfbb11b8fcc7" default))
  '(highlight-indent-guides-method 'bitmap)
  '(ispell-dictionary nil)
  '(lsp-ui-doc-show-with-cursor nil))
