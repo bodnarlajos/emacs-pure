9e9b980584ca6ea3754a6aa085ae25351da3f1bd
Lajos Bodnar <lbodnar@wfscorp.com>
2021-09-09 15:34:13 +0200

    Set the font to Iosevka

--- custom.el
+++ custom.el
@@ -20,4 +20,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Source Code Pro" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
+ '(default ((t (:family "Iosevka" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
