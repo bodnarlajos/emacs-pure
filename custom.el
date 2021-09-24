da8be52218e9435b5a43dbe3efb15c43312d9c14
Bodnar Lajos <bodnarlajoska@gmail.com>
2021-09-24 22:28:35 +0200

    Change custom

--- custom.el
+++ custom.el
@@ -6,7 +6,6 @@
  '(blink-cursor-mode nil)
  '(consult-buffer-filter
 	 '("\\` " "\\`\\*Completions\\*\\'" "\\`\\*Flymake log\\*\\'" "\\`\\*Semantic SymRef\\*\\'" "\\`\\*Messages\\*\\'" "\\`\\*straight.*\\*\\'" "\\`\\*tramp/.*\\*\\'"))
- '(cua-mode t nil (cua-base))
  '(custom-safe-themes
 	 '("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "a99fb53a1d22ce353cab8db2fe59353781c13a4e1d90455f54f7e60c061bc9f4" "58fb295e041032fd7a61074ca134259dfdef557ca67d37c4240dbfbb11b8fcc7" default))
  '(highlight-indent-guides-method 'bitmap)
