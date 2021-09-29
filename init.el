4be5875e95fc31ce28e9b8c6c3eb4f658d82c5bf
Lajos Bodnar <lbodnar@wfscorp.com>
2021-09-29 17:12:22 +0200

    Remove mini-frame

--- init.el
+++ init.el
@@ -41,7 +41,6 @@
 (straight-use-package 'selectrum-prescient)
 (straight-use-package 'marginalia)
 (straight-use-package 'consult)
-(straight-use-package 'mini-frame)
 (straight-use-package 'rg)
 (straight-use-package 'undo-tree)
 (straight-use-package 'smex)
@@ -55,7 +54,6 @@
 (selectrum-prescient-mode +1)
 (prescient-persist-mode +1)
 (marginalia-mode +1)
-(mini-frame-mode +1)
 (global-undo-tree-mode)
 (ctrlf-mode +1)
 (show-paren-mode +1)
@@ -114,12 +112,8 @@
   ;; it's not loaded yet, so add our bindings to the load-hook
   (add-hook 'dired-load-hook 'my-dired-init))
 
-;; mini-frame and selectrum config
+;; selectrum config
 (custom-set-variables
- '(mini-frame-color-shift-step 12)
- '(mini-frame-internal-border-color "forest green")
- '(mini-frame-mode t)
- '(mini-frame-show-parameters '((top . 20) (width . 0.7) (left . 0.5) (height . 26)))
  '(selectrum-max-window-height 25))
 
 ;; Better scrolling
