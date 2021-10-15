1f8201968d855c695e7e1006b4b34b11ed0cf9aa
Lajos Bodnar <lbodnar@wfscorp.com>
2021-10-15 11:48:06 +0200

    Fix layout resize bottom panel

--- custom.el
+++ custom.el
@@ -8,7 +8,7 @@
 	 '("\\` " "\\`\\*Completions\\*\\'" "\\`\\*Flymake log\\*\\'" "\\`\\*Semantic SymRef\\*\\'" "\\`\\*Messages\\*\\'" "\\`\\*straight.*\\*\\'" "\\`\\*tramp/.*\\*\\'"))
  '(cua-mode t nil (cua-base))
  '(custom-safe-themes
-	 '("8b9d07b01f2a9566969c2049faf982cab6a4b483dd43de7fd6a016bb861f7762" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "a99fb53a1d22ce353cab8db2fe59353781c13a4e1d90455f54f7e60c061bc9f4" "58fb295e041032fd7a61074ca134259dfdef557ca67d37c4240dbfbb11b8fcc7" default))
+	 '("874dea847a30db30ad2aef7ae3c0d6866ee1dbe9f02d8e40d434301cee47120d" "8b9d07b01f2a9566969c2049faf982cab6a4b483dd43de7fd6a016bb861f7762" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "a99fb53a1d22ce353cab8db2fe59353781c13a4e1d90455f54f7e60c061bc9f4" "58fb295e041032fd7a61074ca134259dfdef557ca67d37c4240dbfbb11b8fcc7" default))
  '(highlight-indent-guides-method 'bitmap)
  '(ispell-dictionary nil)
  '(lsp-ui-doc-show-with-cursor nil)
@@ -17,7 +17,7 @@
  '(mini-frame-internal-border-color "forest green")
  '(mini-frame-mode t)
  '(mini-frame-show-parameters '((top . 20) (width . 0.7) (left . 0.5) (height . 25)))
- '(selectrum-max-window-height 15)
+ '(selectrum-max-window-height 25)
  '(show-paren-mode t)
  '(tool-bar-mode nil)
  '(warning-suppress-types '((comp))))
