e144f6884a4a25b39c9e9f384e4664f76807cf26
Bodnar Lajos <bodnarlajoska@gmail.com>
2021-11-04 12:35:45 +0100

    Clean Custom.el

--- custom.el
+++ custom.el
@@ -1,20 +1,3 @@
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(consult-buffer-filter
-	 '("\\` " "\\`\\*Completions\\*\\'" "\\`\\*Flymake log\\*\\'" "\\`\\*Semantic SymRef\\*\\'" "\\`\\*Messages\\*\\'" "\\`\\*straight.*\\*\\'" "\\`\\*tramp/.*\\*\\'"))
- '(cua-mode t nil (cua-base))
- '(custom-safe-themes
-	 '("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "a99fb53a1d22ce353cab8db2fe59353781c13a4e1d90455f54f7e60c061bc9f4" "58fb295e041032fd7a61074ca134259dfdef557ca67d37c4240dbfbb11b8fcc7" default))
- '(highlight-indent-guides-method 'bitmap)
- '(ispell-dictionary nil)
- '(lsp-ui-doc-show-with-cursor nil)
- '(menu-bar-mode nil)
- '(show-paren-mode t)
- '(tool-bar-mode nil))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
