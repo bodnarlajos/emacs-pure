f317d513563dac827f343213571a948a9098d4cd
Lajos Bodnar <lbodnar@wfscorp.com>
2021-11-09 13:41:21 +0100

    Custom changes

--- custom.el
+++ custom.el
@@ -10,8 +10,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-	 '("f58f766170b04a1c73d4cf152eac9bb6e317ceb6f5a2f92dcc3e300c9e28eb8f" "e8bbe184ba98a77000a75b261993c3aa4d24e0d1224a96784315b0dae1d6f64a" "b616b5b7a808c4d46a90549bca2f4dcb5bff4f0e04ddaece5611b186f7e9de53" default))
+	 '("516eda4b2938fe4ea7a5a2f10029fd6e73848e6b3a6c95c71d518266de854aa1" "f58f766170b04a1c73d4cf152eac9bb6e317ceb6f5a2f92dcc3e300c9e28eb8f" "e8bbe184ba98a77000a75b261993c3aa4d24e0d1224a96784315b0dae1d6f64a" "b616b5b7a808c4d46a90549bca2f4dcb5bff4f0e04ddaece5611b186f7e9de53" default))
  '(ediff-custom-diff-program "C:/Program Files/Git/usr/bin/diff3.exe")
  '(ediff-diff-program "C:/Program Files/Git/usr/bin/diff.exe")
- '(ediff-diff3-program "C:/Program Files/Git/usr/bin/diff3.exe")
- '(selectrum-max-window-height 15))
+ '(ediff-diff3-program "C:/Program Files/Git/usr/bin/diff3.exe"))
