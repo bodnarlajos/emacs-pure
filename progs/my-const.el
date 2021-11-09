d4deacc195f93195a2a134364ed76d210380a8c7
Lajos Bodnar <lbodnar@wfscorp.com>
2021-11-09 16:15:53 +0100

    Set org mode height.

--- progs/my-const.el
+++ progs/my-const.el
@@ -28,7 +28,14 @@
 		(setq org-todo-keywords
 					'((sequence "TODO" "IN-PROGRESS" "INFO-NEEDED" "TESTING" "|" "DONE" "DELEGATED" "FAILED"))
 					org-support-shift-select t
-					org-log-done t))	
+					org-log-done t)
+		(custom-set-faces
+		 '(org-level-1 ((t (:inherit outline-1 :height 2.0))))
+		 '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
+		 '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+		 '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+		 '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+		 ))
 	;; The window initial size
 	;; specified size
   ;; (add-to-list 'default-frame-alist '(width . 140))
