1e1a914007e266201c9b5b0a23ca907e2b02061d
Bodnar Lajos <bodnarlajoska@gmail.com>
2021-11-22 07:25:15 +0100

    Add custom.el

--- custom.el
+++ custom.el
@@ -10,5 +10,4 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-	 '("1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" default))
- '(selectrum-max-window-height 15))
+	 '("90a6f96a4665a6a56e36dec873a15cbedf761c51ec08dd993d6604e32dd45940" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" default)))
