0865db3310b76fa7bc9c3c7e57cc9a96020f3332
Bodnar Lajos <bodnarlajoska@gmail.com>
2022-01-13 16:53:50 +0100

    Add lsp support to corfu

--- progs/my-dev.el
+++ progs/my-dev.el
@@ -24,8 +24,34 @@
 (add-hook 'my/dev-hook (lambda ()
 												 (straight-use-package 'lsp-mode)
 												 (straight-use-package 'lsp-ui)
-												 (custom-set-variables
-													'(lsp-ui-doc-show-with-cursor t))))
+												 (setq lsp-headerline-breadcrumb-enable nil)
+												 (setq lsp-completion-provider :none)
+												 (setq lsp-ui-imenu-enable t)
+												 (setq lsp-ui-sideline-enable t)
+												 (setq lsp-ui-sideline-show-diagnostics t)
+												 (setq lsp-ui-sideline-show-hover nil)
+												 (setq lsp-enable-snippet nil)
+												 (setq lsp-ui-sideline-show-symbol nil)
+												 (setq lsp-ui-sideline-show-code-actions t)
+												 (setq lsp-ui-sideline-diagnostic-max-lines 5)
+												 (setq lsp-ui-peek-enable nil)
+												 (setq lsp-ui-doc-enable t)
+												 (setq lsp-ui-doc-show-with-mouse t)
+												 (setq lsp-ui-doc-show-with-cursor nil)
+	
+												 (add-hook 'lsp-mode-hook
+																	 (lambda ()
+																		 (setf (caadr ;; Pad before lsp modeline error info
+																						(assq 'global-mode-string mode-line-misc-info))
+																					 " ")))
+
+												 (add-hook 'lsp-completion-mode-hook
+																	 (lambda ()
+																		 (setf (alist-get 'lsp-capf completion-category-defaults) '((styles . (orderless))))))
+
+												 (straight-use-package 'eldoc-box)
+												 (global-eldoc-mode +1)
+												 (add-hook 'lsp-mode-hook 'eldoc-box-hover-mode +1)))
 ;; end of the ide-mode customization
 
 ;; the custom prog mode
