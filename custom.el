ea1174f898276bdf32613931a6809f6cf4ef2bbf
Lajos Bodnar <lbodnar@wfscorp.com>
2022-01-14 15:33:23 +0100

    Add custom.el (font changed)

--- custom.el
+++ custom.el
@@ -3,11 +3,39 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Iosevka" :foundry "outline" :slant normal :weight normal :height 98 :width normal)))))
+ '(default ((t (:family "Roboto Mono" :foundry "outline" :slant normal :weight normal :height 98 :width normal))))
+ '(magit-section-highlight ((t (:background nil))))
+ '(mode-line ((t (:height 1.2))))
+ '(org-fontify-todo-headline t)
+ '(org-fontify-whole-heading-line t)
+ '(org-hide-leading-stars t)
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5 :box nil))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.3 :box nil))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2 :box nil))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.1 :box nil))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0 :box nil)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(blink-cursor-mode nil)
+ '(corfu-auto t)
+ '(corfu-auto-prefix 2)
+ '(cua-mode t)
  '(custom-safe-themes
-	 '("ad4e739865f9f5dbffd8793645df575c0621f02ba1bba29eff45c3ecbd1bb4a6" "607e770f7067e17efc79727aea3d1275e03999f2d6f88d3d8025cd51621f5b69" "516eda4b2938fe4ea7a5a2f10029fd6e73848e6b3a6c95c71d518266de854aa1" "f58f766170b04a1c73d4cf152eac9bb6e317ceb6f5a2f92dcc3e300c9e28eb8f" "e8bbe184ba98a77000a75b261993c3aa4d24e0d1224a96784315b0dae1d6f64a" "b616b5b7a808c4d46a90549bca2f4dcb5bff4f0e04ddaece5611b186f7e9de53" default)))
+	 '("ad4e739865f9f5dbffd8793645df575c0621f02ba1bba29eff45c3ecbd1bb4a6" "607e770f7067e17efc79727aea3d1275e03999f2d6f88d3d8025cd51621f5b69" "516eda4b2938fe4ea7a5a2f10029fd6e73848e6b3a6c95c71d518266de854aa1" "f58f766170b04a1c73d4cf152eac9bb6e317ceb6f5a2f92dcc3e300c9e28eb8f" "e8bbe184ba98a77000a75b261993c3aa4d24e0d1224a96784315b0dae1d6f64a" "b616b5b7a808c4d46a90549bca2f4dcb5bff4f0e04ddaece5611b186f7e9de53" default))
+ '(ediff-custom-diff-program "C:/Program Files/Git/usr/bin/diff3.exe")
+ '(ediff-diff-program "C:/Program Files/Git/usr/bin/diff.exe")
+ '(ediff-diff3-program "C:/Program Files/Git/usr/bin/diff3.exe")
+ '(epg-gpg-home-directory "c:/Users/lbodnar/AppData/Roaming/gnupg")
+ '(epg-gpg-program "c:/Program Files (x86)/gnupg/bin/gpg.exe")
+ '(epg-gpgconf-program "c:/Program Files (x86)/gnupg/bin/gpgconf.exe")
+ '(highlight-indent-guides-method 'bitmap)
+ '(selectrum-max-window-height 15)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil)
+ '(web-mode-code-indent-offset 2)
+ '(web-mode-css-indent-offset 2)
+ '(web-mode-markup-indent-offset 2)
+ '(web-mode-sql-indent-offset 2))
