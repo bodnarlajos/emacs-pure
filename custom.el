60c2f2e3d8954ac09d560c367c6457cf8f94a1d4
Bodnar Lajos <bodnarlajoska@gmail.com>
2022-01-20 21:33:36 +0100

    Add fix in custom.el

--- custom.el
+++ custom.el
@@ -3,25 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Iosevka" :foundry "CYEL" :slant normal :weight normal :height 128 :width normal))))
- '(magit-section-highlight ((t (:background nil))))
- '(mode-line ((t (:height 1.2))))
- '(org-fontify-todo-headline t)
- '(org-fontify-whole-heading-line t)
- '(org-hide-leading-stars t)
- '(org-level-1 ((t (:inherit outline-1 :height 1.5 :box nil))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.3 :box nil))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.2 :box nil))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.1 :box nil))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.0 :box nil)))))
+ '(default ((t (:family "Roboto Mono" :foundry "GOOG" :slant normal :weight normal :height 120 :width normal)))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-	 '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "90a6f96a4665a6a56e36dec873a15cbedf761c51ec08dd993d6604e32dd45940" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" default))
- '(web-mode-code-indent-offset 2)
- '(web-mode-css-indent-offset 2)
- '(web-mode-markup-indent-offset 2)
- '(web-mode-sql-indent-offset 2))
+	 '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "90a6f96a4665a6a56e36dec873a15cbedf761c51ec08dd993d6604e32dd45940" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" default)))
