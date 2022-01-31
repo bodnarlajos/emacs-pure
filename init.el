8060d16cab0c3a77a0afb02542a19b67328f12ba
Lajos Bodnar <lbodnar@wfscorp.com>
2022-01-31 17:09:26 +0100

    Add embark

--- init.el
+++ init.el
@@ -53,7 +53,6 @@
 (straight-use-package 'orderless)
 (straight-use-package 'marginalia)
 ;; (straight-use-package 'mini-frame)
-(straight-use-package 'consult)
 (straight-use-package 'rg)
 (straight-use-package 'undo-tree)
 (straight-use-package 'anzu)
@@ -69,6 +68,38 @@
 						 :straight t
 						 :config
 						 (setq use-package-ensure t))
+
+(use-package consult
+	:straight t
+	:bind
+	("M-s s" . consult-ripgrep))
+
+(use-package embark
+	:straight t
+  :bind
+  (("C-." . embark-act)         ;; pick some comfortable binding
+   ("C-;" . embark-dwim)        ;; good alternative: M-.
+   ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
+  :init
+  ;; Optionally replace the key help with a completing-read interface
+  (setq prefix-help-command #'embark-prefix-help-command)
+  :config
+  ;; Hide the mode line of the Embark live/completions buffers
+  (add-to-list 'display-buffer-alist
+               '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
+                 nil
+                 (window-parameters (mode-line-format . none)))))
+
+;; Consult users will also want the embark-consult package.
+(use-package embark-consult
+  :ensure t
+	:straight t
+  :after (embark consult)
+  :demand t ; only necessary if you have the hook below
+  ;; if you want to have consult previews as you move around an
+  ;; auto-updating embark collect buffer
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
 
 (use-package corfu
 						 :straight t
