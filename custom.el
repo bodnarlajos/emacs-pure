089fd00efc9759939a4f13b36c1b3963f944d0d1
lajos bodnar <bodnarlajos76@gmail.com>
2022-02-18 07:01:47 +0100

    Cleanup the custom.el

--- custom.el
+++ custom.el
@@ -3,44 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 120 :width normal))))
- '(magit-diff-hunk-heading ((t (:foreground "orange"))))
- '(mode-line ((t (:height 1.1 :font-family "Monospace" :font-size 10))))
- '(org-fontify-todo-headline t)
- '(org-fontify-whole-heading-line t)
- '(org-hide-leading-stars t)
- '(org-level-1 ((t (:inherit outline-1 :height 1.5 :box nil))))
- '(org-level-2 ((t (:inherit outline-2 :height 1.3 :box nil))))
- '(org-level-3 ((t (:inherit outline-3 :height 1.2 :box nil))))
- '(org-level-4 ((t (:inherit outline-4 :height 1.1 :box nil))))
- '(org-level-5 ((t (:inherit outline-5 :height 1.0 :box nil)))))
+ '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 120 :width normal)))))
 (custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(blink-cursor-mode nil)
- '(css-indent-offset 2)
- '(cua-mode t)
- '(custom-enabled-themes '(tsdh-dark))
  '(custom-safe-themes
 	 '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "90a6f96a4665a6a56e36dec873a15cbedf761c51ec08dd993d6604e32dd45940" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" default))
- '(highlight-indent-guides-method 'bitmap)
- '(js-indent-level 2)
- '(lsp-clients-angular-language-server-command
-	 '("node" "/home/lbodnar/.vscode-oss/extensions/angular.ng-template-13.2.3/server" "--logToConsole" "--ngProbeLocations" "/home/lbodnar/.vscode-oss/extensions/angular.ng-template-13.2.3,/home/lbodnar/Projects/courses" "--includeAutomaticOptionalChainCompletions" "--includeCompletionsWithSnippetText" "--tsProbeLocations" ",/home/lbodnar/.vscode-oss/extensions/angular.ng-template-13.2.3,/home/lbodnar/Projects/courses" "--stdio"))
- '(lsp-clients-typescript-tls-path "/usr/lib/node_modules/typescript-language-server")
- '(lsp-disabled-clients '(eslint html-ls))
- '(lsp-eslint-server-command
-	 '("node" "/usr/lib/node_modules/eslint/bin/eslint.js" "--stdin"))
- '(lsp-ui-imenu-enable nil)
- '(lsp-ui-peek-enable nil)
- '(lsp-ui-sideline-ignore-duplicate t)
- '(lsp-ui-sideline-show-code-actions t)
- '(lsp-ui-sideline-show-hover t)
- '(lsp-ui-sideline-update-mode 'line)
- '(tool-bar-mode nil)
- '(web-mode-code-indent-offset 2)
- '(web-mode-css-indent-offset 2)
- '(web-mode-markup-indent-offset 2)
- '(web-mode-sql-indent-offset 2))
+ '(lsp-clients-typescript-tls-path
+	 "/usr/bin/node /usr/lib/node_modules/typescript-language-server")
+ '(lsp-disabled-clients '(eslint html-ls)))
