898cd37cf1f84c955d89edddc3d5075c8998a7e6
lajos bodnar <bodnarlajos76@gmail.com>
2022-02-18 10:55:20 +0100

    add custom.el

--- custom.el
+++ custom.el
@@ -3,8 +3,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 120 :width normal)))))
+ '(default ((t (:family "Hack" :foundry "SRC" :slant normal :weight normal :height 120 :width normal))))
+ '(org-fontify-todo-headline t)
+ '(org-fontify-whole-heading-line t)
+ '(org-hide-leading-stars t)
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5 :box nil))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.3 :box nil))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2 :box nil))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.1 :box nil))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0 :box nil)))))
 (custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(modus-vivendi))
  '(custom-safe-themes
 	 '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "90a6f96a4665a6a56e36dec873a15cbedf761c51ec08dd993d6604e32dd45940" "1704976a1797342a1b4ea7a75bdbb3be1569f4619134341bd5a4c1cfb16abad4" "d268b67e0935b9ebc427cad88ded41e875abfcc27abd409726a92e55459e0d01" default))
  '(lsp-clients-typescript-tls-path
