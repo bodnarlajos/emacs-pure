b0e20927749aeb2e005e7a5f2674abab1f2eff92
Lajos Bodnar <lbodnar@wfscorp.com>
2022-02-23 09:02:38 +0100

    Add ripgrep search in Smart mode

---
+++ custom.el
@@ -0,0 +1,23 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+	 '("8f59356bb9284b480e2d2a55caf14eafcbd8a2679247b983230e7ba044e3b5c9" default))
+ '(find-file-rg-arguments "--follow -S"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Hack" :foundry "outline" :slant normal :weight regular :height 98 :width normal))))
+ '(mini-popup-background ((t (:background "white"))))
+ '(org-fontify-todo-headline t)
+ '(org-fontify-whole-heading-line t)
+ '(org-hide-leading-stars t)
+ '(org-level-1 ((t (:inherit outline-1 :height 1.5 :box nil))))
+ '(org-level-2 ((t (:inherit outline-2 :height 1.3 :box nil))))
+ '(org-level-3 ((t (:inherit outline-3 :height 1.2 :box nil))))
+ '(org-level-4 ((t (:inherit outline-4 :height 1.1 :box nil))))
+ '(org-level-5 ((t (:inherit outline-5 :height 1.0 :box nil)))))
