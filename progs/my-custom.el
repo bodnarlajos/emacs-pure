2c1e618eca6b45ced2d705fac0a193b61155776c
Lajos Bodnar <bodnarlajos76@gmail.com>
2022-07-30 12:56:57 +0200

    Add new my-custom.el (tab-bar, tab-line, doom-modeline)

--- progs/my-custom.el
+++ progs/my-custom.el
@@ -5,9 +5,16 @@
  '(blink-cursor-mode nil)
  '(column-number-mode t)
  '(css-indent-offset 2)
+ '(doom-modeline-height 36)
  '(standard-indent 2))
 
 (custom-set-faces
+ '(tab-bar ((t (:inherit mode-line))))
+ '(tab-bar-tab-inactive ((t (:inherit mode-line-inactive))))
+ '(tab-line ((t (:inherit mode-line))))
+ '(tab-line-tab ((t (:inherit tab-line))))
+ '(tab-line-tab-current ((t (:inherit tab-line-tab))))
+ '(tab-line-tab-inactive ((t (:inherit mode-line-inactive))))
  '(fixed-pitch ((t (:inherit default))))
  '(fixed-pitch-serif ((t (:inherit default)))))
 
