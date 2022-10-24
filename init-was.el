2b0f0304e70cddbb854765f15a0a4ba72e26b8bb
Lajos Bodnar <bodnarlajos76@gmail.com>
2022-10-24 06:46:29 +0200

    First step to clean it up

---
+++ init-was.el
@@ -0,0 +1,53 @@
+;; -*- lexical-binding: t -*-
+
+;; (require 'package)
+;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+;; ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
+;; ;; and `package-pinned-packages`. Most users will not need or want to do this.
+;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;; (package-initialize)
+
+(let ((my-load-file
+       (expand-file-name (concat user-emacs-directory "progs"))))
+  (add-to-list 'load-path my-load-file))
+
+(require 'my-const)
+
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/bodnarlajos/straight.el/master/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+(setq straight-check-for-modifications nil
+      straight-use-package-by-default t
+      use-package-always-defer t
+      completion-ignore-case t
+      read-file-name-completion-ignore-case t
+      read-buffer-completion-ignore-case t)
+
+(require 'my-defun)
+(require 'my-keys)
+
+(straight-use-package 'use-package)
+(use-package use-package
+  :straight t
+  :config
+  (setq use-package-always-ensure t))
+
+(require 'my-core)
+
+(require 'server)
+(when (not (server-running-p))
+	(server-start)
+	(message "Server mode started"))
+
+;; activate config
+(my/run/my-config)
--- init.el
+++ init.el
@@ -1,53 +1,65 @@
-;; -*- lexical-binding: t -*-
+(tool-bar-mode -1)
+(setq inhibit-startup-screen t
+      visible-bell t)
+(customize-set-variable 'completion-cycle-threshold 3)
+(customize-set-variable 'tab-always-indent 'complete)
+(customize-set-variable 'completion-category-overrides
+                        '((file (styles . (partial-completion)))))
+(customize-set-variable 'completions-detailed t)
+(fido-vertical-mode 1)               ; fido-vertical-mode is
+(electric-pair-mode 1) ; auto-insert matching bracket
+(show-paren-mode 1)    ; turn on paren match highlighting
+(winner-mode 1)
 
-;; (require 'package)
-;; (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-;; ;; Comment/uncomment this line to enable MELPA Stable if desired.  See `package-archive-priorities`
-;; ;; and `package-pinned-packages`. Most users will not need or want to do this.
-;; (add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
-;; (package-initialize)
+(add-to-list 'display-buffer-alist
+             '("\\*Help\\*"
+               (display-buffer-reuse-window display-buffer-pop-up-window)
+               (inhibit-same-window . t)))
 
-(let ((my-load-file
-       (expand-file-name (concat user-emacs-directory "progs"))))
-  (add-to-list 'load-path my-load-file))
+(add-to-list 'display-buffer-alist
+             '("\\*Completions\\*"
+               (display-buffer-reuse-window display-buffer-pop-up-window)
+               (inhibit-same-window . t)
+               (window-height . 10)))
 
-(require 'my-const)
-
-(defvar bootstrap-version)
-(let ((bootstrap-file
-       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-      (bootstrap-version 5))
-  (unless (file-exists-p bootstrap-file)
-    (with-current-buffer
-        (url-retrieve-synchronously
-         "https://raw.githubusercontent.com/bodnarlajos/straight.el/master/install.el"
-         'silent 'inhibit-cookies)
-      (goto-char (point-max))
-      (eval-print-last-sexp)))
-  (load bootstrap-file nil 'nomessage))
-
-(setq straight-check-for-modifications nil
-      straight-use-package-by-default t
-      use-package-always-defer t
-      completion-ignore-case t
-      read-file-name-completion-ignore-case t
-      read-buffer-completion-ignore-case t)
-
-(require 'my-defun)
-(require 'my-keys)
-
-(straight-use-package 'use-package)
-(use-package use-package
-  :straight t
-  :config
-  (setq use-package-always-ensure t))
-
-(require 'my-core)
-
-(require 'server)
-(when (not (server-running-p))
-	(server-start)
-	(message "Server mode started"))
-
-;; activate config
-(my/run/my-config)
+(customize-set-variable 'switch-to-buffer-in-dedicated-window 'pop)
+(customize-set-variable 'switch-to-buffer-obey-display-actions t)
+(repeat-mode 1)
+(customize-set-variable 'ibuffer-movement-cycle nil)
+(customize-set-variable 'ibuffer-old-time 24)
+(global-set-key [remap list-buffers] #'ibuffer-list-buffers)
+(customize-set-variable 'global-auto-revert-non-file-buffers t)
+(global-auto-revert-mode 1)
+(delete-selection-mode)
+(setq-default indent-tabs-mode nil)
+(if (boundp 'use-short-answers)
+    (setq use-short-answers t)
+  (advice-add 'yes-or-no-p :override #'y-or-n-p))
+(add-hook 'after-init-hook #'recentf-mode)
+(customize-set-variable 'recentf-save-file
+                        (expand-file-name "recentf" user-emacs-directory))
+(customize-set-variable 'kill-do-not-save-duplicates t)
+(setq auto-window-vscroll nil)
+(customize-set-variable 'fast-but-imprecise-scrolling t)
+(customize-set-variable 'scroll-conservatively 101)
+(customize-set-variable 'scroll-margin 0)
+(customize-set-variable 'scroll-preserve-screen-position t)
+(setq-default bidi-paragraph-direction 'left-to-right)
+(setq-default bidi-inhibit-bpa t)
+(global-so-long-mode 1)
+(add-hook 'after-save-hook #'executable-make-buffer-file-executable-if-script-p)
+(savehist-mode 1)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(completion-styles '(substring basic partial-completion emacs22))
+ '(org-adapt-indentation 'headline-data)
+ '(org-startup-indented t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 120 :width normal :foundry "JB" :family "JetBrains Mono")))))
