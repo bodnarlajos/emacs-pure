ed713d83756dfc46fcdcdef2b3895f8192159f70
Lajos Bodnar <bodnarlajos76@gmail.com>
2022-10-28 21:59:20 +0200

    Add angular-ls to eglot

--- lisp/eglot-lsp.el
+++ lisp/eglot-lsp.el
@@ -4,4 +4,14 @@
 (straight-use-package 'lsp-haskell)
 (straight-use-package 'lsp-javascript-typescript)
 
+(with-eval-after-load 'eglot
+  (add-to-list 'eglot-server-programs
+               '(html-mode "node"
+                           "/home/lbodnar/node_modules/@angular/language-server"
+                           "--ngProbeLocations"
+                           "/home/lbodnar/node_modules"
+                           "--tsProbeLocations"
+                           "/home/lbodnar/node_modules"
+                           "--stdio")))
+
 (provide 'eglot-lsp)
