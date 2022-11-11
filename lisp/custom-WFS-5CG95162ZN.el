9951ae5b0edb3da109957ac00e237e370f1fe13a
Lajos Bodnar <lbodnar@wfscorp.com>
2022-11-11 10:09:55 +0100

    Remove default's attributes ...

--- lisp/custom-WFS-5CG95162ZN.el
+++ lisp/custom-WFS-5CG95162ZN.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :extend nil :stipple nil :background "white" :foreground "black" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight regular :height 100 :width normal :foundry "JB" :family "JetBrains Mono"))))
+ '(default ((t (:slant normal :weight regular :height 100 :width normal :foundry "JB" :family "JetBrains Mono"))))
  '(variable-pitch ((t (:slant normal :weight bold :height 100 :foundry "outline" :family "Roboto Mono")))))
 
 (custom-set-variables
