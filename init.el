b9513f54479051f6b57ae8102e12d6dec1565b61
Bodnar Lajos <41907523+bodnarlajos@users.noreply.github.com>
2023-01-11 12:10:55 +0100

    add mode-line format

--- init.el
+++ init.el
@@ -156,6 +156,51 @@
  '(tab-bar-tab ((t (:inherit mode-line :box (:line-width (2 . 6) :style flat-button)))))
  '(tab-bar-tab-inactive ((t (:inherit mode-line-inactive)))))
 
+(setq-default mode-line-format
+              (list
+
+               ;; day and time
+               '(:eval (propertize (format-time-string " %b %d %H:%M ")
+                                   'face 'font-lock-builtin-face))
+
+
+               '(:eval (propertize (substring vc-mode 5)
+                                   'face 'font-lock-warning-face))
+
+               ;; the buffer name; the file name as a tool tip
+               '(:eval (propertize " %b "
+                                   'face
+                                   (let ((face (buffer-modified-p)))
+                                     (if face 'font-lock-warning-face
+                                       'font-lock-type-face))
+                                   'help-echo (buffer-file-name)))
+
+               ;; line and column
+               " (" ;; '%02' to set to 2 chars at least; prevents flickering
+               (propertize "%02l" 'face 'font-lock-keyword-face) ","
+               (propertize "%02c" 'face 'font-lock-keyword-face)
+               ") "
+
+               ;; relative position, size of file
+               " ["
+               (propertize "%p" 'face 'font-lock-constant-face) ;; % above top
+               "/"
+               (propertize "%I" 'face 'font-lock-constant-face) ;; size
+               "] "
+
+               ;; spaces to align right
+               ;; '(:eval (propertize
+               ;;          " " 'display
+               ;;          `((space :align-to (- (+ right right-fringe right-margin)
+               ;;                                ,(+ 3 (string-width (if (listp mode-name) (car mode-name) mode-name))))))))
+
+                                        ;(propertize org-mode-line-string 'face '(:foreground "#5DD8FF"))
+
+               ;; the current major mode
+               (propertize (string-replace "-mode" ""  (symbol-name major-mode)) 'face 'font-lock-string-face)
+               ;;minor-mode-alist
+               ))
+
 (require 'server)
 (unless (server-running-p)
   (setq frame-title-format "Server [%b]")
