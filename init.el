c2641a37aea640dd5dd3c953d4dbdd74b1071a17
Lajos Bodnar <bodnarlajoska@gmail.com>
2023-02-09 05:41:29 +0100

    Fix tab-bar style

--- init.el
+++ init.el
@@ -103,9 +103,8 @@
 (add-to-list 'load-path (expand-file-name "~/.emacs.d/lisp"))
 (require 'defuns)
 (require 'extra-packages)
-(require 'keys)
-;; (require 'eglot-lsp)
 (require 'extra-lsp)
+(require 'keys)
 
 (custom-set-variables
  '(next-error-recenter '(4)))
@@ -154,6 +153,13 @@
 (setq tab-bar-tab-name-truncated-max 30)
 (setq tab-bar-format '(tab-bar-format-tabs tab-bar-separator))
 (global-tab-line-mode +1)
+(custom-set-faces
+ '(mode-line ((t (:box (:line-width (8 . 8) :style flat-button)))))
+ '(mode-line-inactive ((t (:box (:line-width (8 . 8) :style flat-button)))))
+ '(tab-bar ((t (:inherit variable-pitch :underline (:color foreground-color :style line :position 0)))))
+ '(tab-bar-tab ((t (:inherit tab-bar :box (:line-width (8 . 8) :style flat-button) :underline (:color foreground-color :style line :position 0)))))
+ '(tab-line ((t (:inherit variable-pitch :underline (:color foreground-color :style line :position 0) :box (:line-width (8 . 8) :style flat-button)))))
+ '(tab-line-tab ((t (:inherit tab-line :style flat-button) :underline (:color foreground-color :style line :position 0)))))
 
 (require 'server)
 (unless (server-running-p)
