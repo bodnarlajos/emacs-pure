d1c9d797cff7046aecc65fb2675da1d589086e27
Lajos Bodnar <bodnarlajoska@gmail.com>
2023-03-14 05:54:56 +0100

    Add embark for consult and use M-e to initiate it.

--- lisp/extra-packages.el
+++ lisp/extra-packages.el
@@ -65,25 +65,24 @@
   :init (global-org-modern-mode +1))
 
 (use-package embark
-  :ensure t
-
   :bind
-  (("C-." . embark-act)         ;; picki some comfortable binding
+  (("M-e" . embark-act)         ;; picki some comfortable binding
    ("C-;" . embark-dwim)        ;; good alternative: M-.
    ("C-h B" . embark-bindings)) ;; alternative for `describe-bindings'
-
   :init
-
   ;; Optionally replace the key help with a completing-read interface
   (setq prefix-help-command #'embark-prefix-help-command)
-
   :config
-
   ;; Hide the mode line of the Embark live/completions buffers
   (add-to-list 'display-buffer-alist
                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
                  nil
                  (window-parameters (mode-line-format . none)))))
+
+(use-package embark-consult
+  :ensure t ; only need to install it, embark loads it after consult if found
+  :hook
+  (embark-collect-mode . consult-preview-at-point-mode))
 
 (use-package dogears
   :init (dogears-mode +1)
