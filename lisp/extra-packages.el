b9ecfc28b20aa9220e86fb4a16ad0dc9536edf07
Lajos Bodnar <bodnarlajoska@gmail.com>
2023-03-29 04:59:21 +0200

    Fix package init

--- lisp/extra-packages.el
+++ lisp/extra-packages.el
@@ -1,25 +1,32 @@
 ;; -*- lexical-binding: t -*-
 
-;; package init
 (require 'package)
-(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
-(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
+
+(setq gnutls-algorithm-priority "NORMAL:-VERS-TLS1.3")
+
+(setq package-archives
+      '(("gnu" . "https://elpa.gnu.org/packages/")
+        ("melpa" . "https://melpa.org/packages/")
+        ("melpa-stable" . "https://stable.melpa.org/packages/")))
+
 (package-initialize)
 
+;; `use-package' makes it easier to install and configure packages
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-(eval-and-compile
-  (setq use-package-always-ensure t
-        use-package-expand-minimally t
-        use-package-verbose nil
-        completion-ignore-case t
-        read-file-name-completion-ignore-case t
-        read-buffer-completion-ignore-case t))
+;; `ensure' that the package is installed
+(setq use-package-always-ensure t)
+
+(setq use-package-verbose nil
+      completion-ignore-case t
+      read-file-name-completion-ignore-case t
+      read-buffer-completion-ignore-case t)
 
 ;; utils
-(use-package rg)
+(use-package rg
+  :ensure t)
 (use-package consult
   :ensure t
   :config
