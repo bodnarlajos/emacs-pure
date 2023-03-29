ac10a78a190e79947c85a5b1b41a3bf2eff9eb9e
Bodnar Lajos <41907523+bodnarlajos@users.noreply.github.com>
2023-03-29 15:38:25 +0200

    Change mode-line face

--- init.el
+++ init.el
@@ -162,8 +162,8 @@
 (setq tab-bar-format '(tab-bar-format-tabs tab-bar-separator))
 ;; (global-tab-line-mode +1)
 (custom-set-faces
- '(mode-line ((t (:box (:line-width (4 . 4) :style flat-button)))))
- '(mode-line-inactive ((t (:box (:line-width (4 . 4) :style flat-button)))))
+ '(mode-line ((t (:box (:line-width (2 . 2) :style flat-button)))))
+ '(mode-line-inactive ((t (:box (:line-width (2 . 2) :style flat-button)))))
  '(tab-bar ((t (:inherit variable-pitch :underline (:color foreground-color :style line :position 0)))))
  '(tab-bar-tab ((t (:inherit tab-bar :box (:line-width (4 . 4) :style flat-button) :underline (:color foreground-color :style line :position 0)))))
  '(tab-line ((t (:inherit variable-pitch :underline (:color foreground-color :style line :position 0) :box (:line-width (4 . 4) :style flat-button)))))
