0b569c24da995231d5b650074514adca59dbfada
Bodnar Lajos <41907523+bodnarlajos@users.noreply.github.com>
2023-05-11 09:49:42 +0200

    Add bonus keys as BLAH

--- early-init.el
+++ early-init.el
@@ -22,5 +22,32 @@
 
 (setq frame-inhibit-implied-resize t)
 
+(add-hook
+ 'after-make-frame-functions
+ (defun setup-blah-keys (frame)
+   (with-selected-frame frame
+     (when (display-graphic-p) ; don't remove this condition, if you want
+                                        ; terminal Emacs to be usable
+       ;; - When you type `Ctrl-i', Emacs see it as `BLAH-i', and NOT as 'Tab'
+       ;; - When you type `Ctrl-m', Emacs see it as `BLAH-m', and NOT as 'Return'
+       ;; - When you type `Ctrl-[', Emacs see it as `BLAH-lsb', and not as 'Esc'.
+       ;;
+       ;; That is,
+       ;;
+       ;; - `Ctrl-i' and 'Tab' keys are different
+       ;; - `Ctrl-m' and 'Return' keys are different
+       ;; - `Ctrl-[' and 'Esc' keys are different
+       ;;
+       ;; The three BLAH keys are the bonus keys.
+       (define-key input-decode-map (kbd "C-i") [BLAH-i])
+       (define-key input-decode-map (kbd "C-[") [BLAH-lsb]) ; left square bracket
+       (define-key input-decode-map (kbd "C-m") [BLAH-m])
+       ;; You can replace `BLAH-' above with `C-' or
+       ;; `CONTROL-', it doesnt' matter.
+       ;;
+       ;; BLAH is merely a symbol / name; feel free to change
+       ;; it to whatever you like .
+       ))))
+
 (provide 'early-init)
 ;;; early-init.el ends here 
