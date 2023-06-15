41adb4de23b2c9e5ff18413915cf132966851d7f
Lajos Bodnar <l.bodnar@biconcepts.at>
2023-06-15 11:18:31 +0200

    Add BIC custom file

--- lisp/custom-BICWS21.el
+++ lisp/custom-BICWS21.el
@@ -3,6 +3,86 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(connection-local-criteria-alist
+   '(((:application tramp :protocol "flatpak")
+      tramp-container-connection-local-default-flatpak-profile)
+     ((:application tramp)
+      tramp-connection-local-default-system-profile tramp-connection-local-default-shell-profile)))
+ '(connection-local-profile-alist
+   '((tramp-container-connection-local-default-flatpak-profile
+      (tramp-remote-path "/app/bin" tramp-default-remote-path "/bin" "/usr/bin" "/sbin" "/usr/sbin" "/usr/local/bin" "/usr/local/sbin" "/local/bin" "/local/freeware/bin" "/local/gnu/bin" "/usr/freeware/bin" "/usr/pkg/bin" "/usr/contrib/bin" "/opt/bin" "/opt/sbin" "/opt/local/bin"))
+     (tramp-connection-local-darwin-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,uid,user,gid,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state=abcde" "-o" "ppid,pgid,sess,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etime,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (euid . number)
+       (user . string)
+       (egid . number)
+       (comm . 52)
+       (state . 5)
+       (ppid . number)
+       (pgrp . number)
+       (sess . number)
+       (ttname . string)
+       (tpgid . number)
+       (minflt . number)
+       (majflt . number)
+       (time . tramp-ps-time)
+       (pri . number)
+       (nice . number)
+       (vsize . number)
+       (rss . number)
+       (etime . tramp-ps-time)
+       (pcpu . number)
+       (pmem . number)
+       (args)))
+     (tramp-connection-local-busybox-ps-profile
+      (tramp-process-attributes-ps-args "-o" "pid,user,group,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "stat=abcde" "-o" "ppid,pgid,tty,time,nice,etime,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (user . string)
+       (group . string)
+       (comm . 52)
+       (state . 5)
+       (ppid . number)
+       (pgrp . number)
+       (ttname . string)
+       (time . tramp-ps-time)
+       (nice . number)
+       (etime . tramp-ps-time)
+       (args)))
+     (tramp-connection-local-bsd-ps-profile
+      (tramp-process-attributes-ps-args "-acxww" "-o" "pid,euid,user,egid,egroup,comm=abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ" "-o" "state,ppid,pgid,sid,tty,tpgid,minflt,majflt,time,pri,nice,vsz,rss,etimes,pcpu,pmem,args")
+      (tramp-process-attributes-ps-format
+       (pid . number)
+       (euid . number)
+       (user . string)
+       (egid . number)
+       (group . string)
+       (comm . 52)
+       (state . string)
+       (ppid . number)
+       (pgrp . number)
+       (sess . number)
+       (ttname . string)
+       (tpgid . number)
+       (minflt . number)
+       (majflt . number)
+       (time . tramp-ps-time)
+       (pri . number)
+       (nice . number)
+       (vsize . number)
+       (rss . number)
+       (etime . number)
+       (pcpu . number)
+       (pmem . number)
+       (args)))
+     (tramp-connection-local-default-shell-profile
+      (shell-file-name . "/bin/sh")
+      (shell-command-switch . "-c"))
+     (tramp-connection-local-default-system-profile
+      (path-separator . ":")
+      (null-device . "/dev/null"))))
  '(css-indent-offset 2)
  '(csv-separators '("," ";" ":"))
  '(custom-safe-themes
@@ -17,7 +97,7 @@
  '(org-startup-indented t)
  '(org-support-shift-select t)
  '(package-selected-packages
-   '(mini-frame doom-modeline goto-chg doom-themes vs-light-theme vs-dark-theme nord-theme powershell forge kind-icon all-the-icons-completion all-the-icons-dired angular-mode anzu cape corfu crux csv-mode diff-hl diminish dogears easy-kill ef-themes embark-consult go-translate haskell-mode hydra js2-mode magit marginalia markdown-mode move-text orderless org-modern org-present plz remember-last-theme rg treesit-auto typescript-mode undo-tree vertico visual-fill-column web-mode which-key yaml-mode))
+   '(magit-delta mini-frame doom-modeline goto-chg doom-themes vs-light-theme vs-dark-theme nord-theme powershell forge kind-icon all-the-icons-completion all-the-icons-dired angular-mode anzu cape corfu crux csv-mode diff-hl diminish dogears easy-kill ef-themes embark-consult go-translate haskell-mode hydra js2-mode magit marginalia markdown-mode move-text orderless org-modern org-present plz remember-last-theme rg treesit-auto typescript-mode undo-tree vertico visual-fill-column web-mode which-key yaml-mode))
  '(tab-first-completion nil)
  '(xref-show-xrefs-function 'consult-xref))
 (custom-set-faces
