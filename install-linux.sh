fef5d726a22e7fd8aeda1c14327b94dd4c6621ac
bodnarlajos <bodnarlajoka@gmail.com>
2023-07-07 08:59:38 +0000

    add emacs-gui

--- install-linux.sh
+++ install-linux.sh
@@ -3,3 +3,6 @@
 # git-delta binary
 wget https://github.com/dandavison/delta/releases/download/latest/git-delta_0.16.5_amd64.deb -O /tmp/git-delta.deb && sudo apt install -y /tmp/git-delta.deb && rm /tmp/git-delta.deb
 
+sudo cp ~/.emacs.d/emacs-gui /usr/bin/
+chmod 755 /usr/bin/emacs-gui
+
